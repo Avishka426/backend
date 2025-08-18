b961c23a1cdc6d3dfd917208126d9755f1a221c1
Avishka <avishkavishmitha2@gmail.com>
2025-08-18 06:46:59 +0530

    bug fix

--- src/config/db.js
+++ src/config/db.js
@@ -14,7 +14,7 @@ export async function connectToDatabase() {
             user_id VARCHAR(255) NOT NULL,
             title VARCHAR(255) NOT NULL,
             amount DECIMAL(10,2) NOT NULL,
-            catagory VARCHAR(255) NOT NULL,
+            category VARCHAR(255) NOT NULL,
             created_at DATE NOT NULL DEFAULT CURRENT_DATE
         )`
 
